3218d7860f7c4a174c8564e631aca496de0ae153
gokakyu <vcs.cellier.florian.283@gmail.com>
2024-12-19 16:19:56 +0000

    feat: clean project to made it public

--- samples-java/user-manager/src/main/java/com/example/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.repository;
-
-import com.example.user.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Long> {
-}
